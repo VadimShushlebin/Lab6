768f580caf485489f143cea48eb11ee9e50008ff
PC <PC@DESKTOP-8MN65CJ>
2025-10-15 15:45:28 +0300

    Добавьте файлы проекта.

---
+++ Lab6.c
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+#define _USE_MATH_DEFINES
+
+#include <math.h>
+int main()
+{
+	int N, sum;
+	setlocale(LC_ALL, "RUS");
+	printf("Введите число N (N<1000): ");
+	scanf("%d", &N);
+	int N1 = N / 100;
+	int N2 = (N / 10) % 10;
+	int N3 = N % 10;
+	sum = N1 + N2 + N3;
+	if (sum % 3 == 0) {
+		printf("Сумма цифр числа %d кратна 3", N);
+	}else {
+		printf("Сумма цифр числа %d не кратна 3",N);
+	}
+}
